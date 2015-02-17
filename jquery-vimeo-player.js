6b354798f53fbf98deaa5251318ca8f15602649a
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2015-02-17 16:05:39 +0100

    Adding main file to see if all is working through Bower n stuff...

---
+++ jquery-vimeo-player.js
@@ -0,0 +1,19 @@
+/*
+ *  jQuery-Vimeo-player - v0.3.0
+ *  Vimeo player for jQuery inspired by Pupunzi's jquery.mb.ytplayer
+ *  https://github.com/FlipFlopInteractive/jquery-vimeo-player
+ *
+ *  Made by Koen Dirk van Esterik
+ *  Under MIT License
+ */
+!( function( $, window, document, navigator ){
+
+    "use strict";
+
+    function initVimeoPlayer(){
+
+        console.log( 'Hello world' );
+    }
+
+
+})( window.jQuery, window, document, navigator );
