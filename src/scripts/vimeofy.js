bc8851df159c65250f63e7903641ddc577545518
klokie <github@klokie.com>
2016-08-16 14:52:08 +0200

    normalized spacing

--- src/scripts/vimeofy.js
+++ src/scripts/vimeofy.js
@@ -6,210 +6,219 @@
  *  Made by Koen Dirk van Esterik
  *  Under MIT License
  */
-!( function( $, window, document, navigator ){
+!(function($, window, document, navigator) {
 
-	'use strict';
+  'use strict';
 
 
-	/**
-	 * Vimeofy settings
-	 * @private
-	 */
-	var pluginName 	= 'vimeofy';
-	var isIOS 		= /iPad|iPhone|iPod/i.test( navigator.userAgent );
-	var isAndroid 	= /Android/i.test( navigator.userAgent );
+  /**
+   * Vimeofy settings
+   * @private
+   */
+  var pluginName = 'vimeofy';
+  var isIOS = /iPad|iPhone|iPod/i.test(navigator.userAgent);
+  var isAndroid = /Android/i.test(navigator.userAgent);
 
 
 
-	/**
-	 * Vimeofy constructor
-	 * @param {HTMLElement} element
-	 * @param {Object|String} path
-	 * @constructor
-	 */
-	function Vimeofy( element, options ){
+  /**
+   * Vimeofy constructor
+   * @param {HTMLElement} element
+   * @param {Object|String} path
+   * @constructor
+   */
+  function Vimeofy(element, options) {
 
-		this.$element 	= $( element );
-		this.url 		= parseUrl( options );
-		this.delay 		= ( options.delay ) ? parseInt( options.delay ) : 0;
+    this.$element = $(element);
+    this.url = parseUrl(options);
+    this.delay = (options.delay) ? parseInt(options.delay) : 0;
 
-		this.init();
-	}
+    this.init();
+  }
 
 
 
-	/**
-	 * Parse string with options
-	 * @param {Object} options
-	 * @returns {String}
-	 * @private
-	 */
-	function parseUrl( options ){
+  /**
+   * Parse string with options
+   * @param {Object} options
+   * @returns {String}
+   * @private
+   */
+  function parseUrl(options) {
 
-		var url 		= options.url.replace( /(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '//player.vimeo.com/video/$1' );
-		var color		= options.color.replace( /^#/, '?color=' );
-		var autoplay	= options.autoplay ? '&amp;autoplay=1' : '&amp;autoplay=0';
-		var loop 		= options.loop ? '&amp;loop=1' : '&amp;loop=0';
+    var url = options.url.replace(/(?:https?:\/\/)?(?:www\.)?(?:vimeo\.com)\/(.+)/g, '//player.vimeo.com/video/$1');
+    var color = options.color.replace(/^#/, '?color=');
+    var autoplay = options.autoplay ? '&amp;autoplay=1' : '&amp;autoplay=0';
+    var loop = options.loop ? '&amp;loop=1' : '&amp;loop=0';
 
-		return url + color + autoplay + loop;
-	}
+    return url + color + autoplay + loop;
+  }
 
 
 
-	/**
-	 * Initialization
-	 * @public
-	 */
-	Vimeofy.prototype.init = function(){
+  /**
+   * Initialization
+   * @public
+   */
+  Vimeofy.prototype.init = function() {
 
-		var vimeofy = this;
-		
-		if( !isIOS && !isAndroid ){
+    var vimeofy = this;
 
-			// Declare player instance
-			vimeofy.$player = $( '<iframe frameborder="0" allowTransparency="true" style="background:transparent;" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>' );
-			vimeofy.$player.attr( 'src', vimeofy.url );
+    if (!isIOS && !isAndroid) {
 
-			// Append player
-			vimeofy.$element.append( vimeofy.$player );
+      // Declare player instance
+      vimeofy.$player = $('<iframe frameborder="0" allowTransparency="true" style="background:transparent;" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+      vimeofy.$player.attr('src', vimeofy.url);
 
-			// Disable visibility, while loading
-			vimeofy.$player.css({ 'visibility': 'hidden' });
+      // Append player
+      vimeofy.$element.append(vimeofy.$player);
 
-			vimeofy.resize();
+      // Disable visibility, while loading
+      vimeofy.$player.css({
+        'visibility': 'hidden'
+      });
 
-			// resize event is available only for 'window',
-			// use another code solutions to detect DOM elements resizing
-			vimeofy.$element.bind( 'resize.' + pluginName, function(){
+      vimeofy.resize();
 
-				vimeofy.resize();
-			});
+      // resize event is available only for 'window',
+      // use another code solutions to detect DOM elements resizing
+      vimeofy.$element.bind('resize.' + pluginName, function() {
 
-			setTimeout( function(){
+        vimeofy.resize();
+      });
 
-				vimeofy.$player.css({ 'visibility': 'visible' });
-				
-			}, this.delay );
-		}
-	};
+      setTimeout(function() {
 
+        vimeofy.$player.css({
+          'visibility': 'visible'
+        });
 
-	/**
-	 * Resize Vimeo player
-	 * @public
-	 */
-	Vimeofy.prototype.resize = function(){
+      }, this.delay);
+    }
+  };
 
-		if( this.$player ){
 
-			var width 		= this.$element.outerWidth();
-			var height 		= this.$element.outerHeight();
-			var margin 		= 24;
-			var overprint 	= 4;
-			var props 		= {};
+  /**
+   * Resize Vimeo player
+   * @public
+   */
+  Vimeofy.prototype.resize = function() {
 
-			props.width 		= width + (( width * margin ) / 100 );
-			props.height 		= Math.ceil(( 9 * width ) / 16 );
-			props.marginTop 	= -(( props.height - height ) / 2 );
-			props.marginLeft 	= -(( width * ( margin / 2 )) / 100 );
+    if (this.$player) {
 
-			if( props.height < height ){
+      var width = this.$element.outerWidth();
+      var height = this.$element.outerHeight();
+      var margin = 24;
+      var overprint = 4;
+      var props = {};
 
-				props.height 		= height + (( height * margin ) / 100 );
-				props.width 		= Math.floor(( 16 * height ) / 9 );
-				props.marginTop 	= -(( height * ( margin / 2 )) / 100 );
-				props.marginLeft 	= -(( props.width - width ) / 2 );
-			}
+      props.width = width + ((width * margin) / 100);
+      props.height = Math.ceil((9 * width) / 16);
+      props.marginTop = -((props.height - height) / 2);
+      props.marginLeft = -((width * (margin / 2)) / 100);
 
-			props.width += overprint;
-			props.height += overprint;
-			props.marginTop -= overprint / 2;
-			props.marginLeft -= overprint / 2;
+      if (props.height < height) {
 
-			this.$player.css({ width: props.width, height: props.height, marginTop: props.marginTop, marginLeft: props.marginLeft });
-		}
-	};
+        props.height = height + ((height * margin) / 100);
+        props.width = Math.floor((16 * height) / 9);
+        props.marginTop = -((height * (margin / 2)) / 100);
+        props.marginLeft = -((props.width - width) / 2);
+      }
 
+      props.width += overprint;
+      props.height += overprint;
+      props.marginTop -= overprint / 2;
+      props.marginLeft -= overprint / 2;
 
+      this.$player.css({
+        width: props.width,
+        height: props.height,
+        marginTop: props.marginTop,
+        marginLeft: props.marginLeft
+      });
+    }
+  };
 
-	/**
-	 * Destroy video background
-	 * @public
-	 */
-	Vimeofy.prototype.destroy = function(){
 
-		this.$element.unbind( pluginName );
 
-		if( this.$player ){
+  /**
+   * Destroy video background
+   * @public
+   */
+  Vimeofy.prototype.destroy = function() {
 
-			this.$player.unbind( pluginName );
-		}
+    this.$element.unbind(pluginName);
 
-		delete $[ pluginName ].lookup[ this.index ];
-		this.$element.removeData( pluginName );
-		this.$player.remove();
-	};
+    if (this.$player) {
 
+      this.$player.unbind(pluginName);
+    }
 
+    delete $[pluginName].lookup[this.index];
+    this.$element.removeData(pluginName);
+    this.$player.remove();
+  };
 
-	/**
-	 * Special plugin object for instances.
-	 * @type {Object}
-	 * @public
-	 */
-	$[ pluginName ] = {
 
-		lookup: []
-	};
 
+  /**
+   * Special plugin object for instances.
+   * @type {Object}
+   * @public
+   */
+  $[pluginName] = {
 
+    lookup: []
+  };
 
-	/**
-	 * Plugin constructor
-	 * @param {Object|String} options
-	 * @returns {JQuery}
-	 * @constructor
-	 */
-	$.fn[ pluginName ] = function( options ){
 
-		var instance;
 
-		this.each( function() {
+  /**
+   * Plugin constructor
+   * @param {Object|String} options
+   * @returns {JQuery}
+   * @constructor
+   */
+  $.fn[pluginName] = function(options) {
 
-			instance = $.data( this, pluginName );
+    var instance;
 
-			if( instance ){
+    this.each(function() {
 
-				// destroy plugin instance if exists
-				instance.destroy();
-			}
+      instance = $.data(this, pluginName);
 
-			// create plugin instance
-			instance = new Vimeofy( this, options );
-			instance.index = $[ pluginName ].lookup.push( instance ) - 1;
-			$.data( this, pluginName, instance );
-		});
+      if (instance) {
 
-		return this;
-	};
+        // destroy plugin instance if exists
+        instance.destroy();
+      }
 
+      // create plugin instance
+      instance = new Vimeofy(this, options);
+      instance.index = $[pluginName].lookup.push(instance) - 1;
+      $.data(this, pluginName, instance);
+    });
 
+    return this;
+  };
 
-	$( document ).ready( function(){
 
-		$( window ).bind( 'resize.' + pluginName, function(){
 
-			for( var len = $[ pluginName ].lookup.length, i = 0, instance; i < len; i++ ){
+  $(document).ready(function() {
 
-				instance = $[ pluginName ].lookup[ i ];
+    $(window).bind('resize.' + pluginName, function() {
 
-				if( instance ){
+      for (var len = $[pluginName].lookup.length, i = 0, instance; i < len; i++) {
 
-					clearTimeout( instance.timer );
-					instance.timer = setTimeout( instance.resize(), 250 );
-				}
-			}
-		});
-	});
+        instance = $[pluginName].lookup[i];
 
-})( window.jQuery, window, document, navigator );
+        if (instance) {
+
+          clearTimeout(instance.timer);
+          instance.timer = setTimeout(instance.resize(), 250);
+        }
+      }
+    });
+  });
+
+})(window.jQuery, window, document, navigator);
